5990c76761c041c7281c90da362f023e8fa196f5
Rikuto <orikutobasuke@gmail.com>
2024-04-15 19:21:39 +0900

    commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import style from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <>
+      <div className={style.sample}>
+        <div></div>
+      </div>
+    </>
+  );
+}
